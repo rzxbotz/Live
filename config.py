f2e0a1ecfcce1bc5804f91e6845b26b0b1ae1270
rzxbotz <rzxbots@gmail.com>
2025-02-27 01:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_ID = int(17427408)
 API_HASH = "9699e632de895e7d566c241615a0e637"
-BOT_TOKEN ="7951751315:AAFGKkOldJPknNrY-mDxnlg5qCNfvGfkWHo"
+BOT_TOKEN ="7391030602:AAHHBkz1f6c9Zc13hGHzE587TKdUcD6Uf_A"
 DB_URI = "mongodb+srv://dudemusic111:dudemusic111@cluster0.df3yis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 CAPTION = "<b>{file_caption}</b>"

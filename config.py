affb3b5562def8ed233ef4d75f5e1b45aad0107b
rzxbotz <rzxbots@gmail.com>
2025-02-27 20:19:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", 17427408))  # Telegram API ID
 API_HASH = os.getenv("API_HASH", "9699e632de895e7d566c241615a0e637")  # Telegram API Hash
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7391030602:AAHHBkz1f6c9Zc13hGHzE587TKdUcD6Uf_A")  # Telegram Bot Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7391030602:AAHOt_evgGTPyN3W2qW5P8fSiOQ_9IHBOVs")  # Telegram Bot Token
 SESSION_NAME = "movie_forward_bot"  # Pyrogram Session Name
 
 SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", -1002492867485))  # Source Channel ID

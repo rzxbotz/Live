2fe26b7e27b38db93cf9d15ae874ecd555e3a90c
rzxbotz <rzxbots@gmail.com>
2025-04-02 16:48:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-SESSION_NAME = "movie_forward_bot"
+API_ID = int(os.getenv("API_ID"))  # Telegram API ID
+API_HASH = os.getenv("API_HASH")  # Telegram API Hash
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Telegram Bot Token
+SESSION_NAME = "movie_forward_bot"  # Pyrogram Session Name
 
-SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", ""))
-DESTINATION_CHANNEL = list(map(int, os.getenv("DESTINATION_CHANNEL", "").replace(" ", ",").split(',')))
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL"))  # Source Channel ID
+DESTINATION_CHANNEL = int(os.getenv("DESTINATION_CHANNEL"))  # Destination Channel ID
+MONGO_URI = os.getenv("MONGO_URI")  # MongoDB Connection URI
 
-MONGO_URI = os.getenv("MONGO_URI", "")
-CAPTION_TEMPLATE = os.getenv("CAPTION_TEMPLATE", "<b>{original_caption}</b>")
+CAPTION_TEMPLATE = os.getenv("CAPTION_TEMPLATE", "<b>{original_caption}</b>")  # Custom Caption Format
 
 MOVIE_REGEX = r"(?i)^(?!.*\b(?:S\d{2}E\d{2}|S\d{2}\s?EP\d{2}|S\d{2}\s?E\d{2}|Season\s?\d+\s?Episode\s?\d+|EP\d+|E\d{2}(-E\d{2,})?|combined|-\sS\d{2}E\d{2}\s-)\b).*(19\d{2}|20\d{2}).*"
